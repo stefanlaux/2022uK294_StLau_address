762e2cdf879f50e48b1d72ad68acc7505ffd6831
StefanLaux <stefan.laux@edu.tbz.ch>
2022-06-02 08:50:48 +0200

    replaced https request with axios

--- Projektarbeit/react-app/src/molecules/LoginForm.tsx
+++ Projektarbeit/react-app/src/molecules/LoginForm.tsx
@@ -1,55 +1,67 @@
+import axios from "axios";
 import React, { useState } from "react";
-import '../css/Login.css'
+import "../css/Login.css";
 
 function LoginForm() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [accessToken, setToken] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [accessToken, setToken] = useState("");
 
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-        event.preventDefault();
-
-        try {
-            const response = await fetch("http://localhost:3030/login", {
-                method: "POST",
-                body: JSON.stringify({
-                    email: email,
-                    password: password
-                }),
-                headers: {
-                    'Content-Type': 'application/json',
-                    Accept: "application/json"
-                }
-            });
-            const token = await response.json();
-            setToken(token.accessToken);
-
-            if (response.status === 400) {
-                alert("Invalid credentials or user does not exist");
-            }
-
-        } catch (error) {
-            console.log(error);
+    try {
+      axios({
+        method: "POST",
+        url: "http://localhost:3030/login",
+        data: {
+          email: email,
+          password: password,
+        },
+      }).then((response) => {
+        setToken(response.data.accessToken);
+        if (response.status === 400) {
+          alert("Invalid credentials or user does not exist");
         }
+      });
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return (
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-                <input value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" type="email" name="email" required />
-                <br />
-                <input value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" type="password" name="password" required />
-                <br />
-                <button className="btn" type="submit">Sign in</button>
-            </form>
-            <div className="divLink">
-                <a className="link" href="/register">Neues Konto erstellen</a>
-            </div>
-        </div>
-    );
-
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          type="email"
+          name="email"
+          required
+        />
+        <br />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          type="password"
+          name="password"
+          required
+        />
+        <br />
+        <button className="btn" type="submit">
+          Sign in
+        </button>
+      </form>
+      <div className="divLink">
+        <a className="link" href="/register">
+          Neues Konto erstellen
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default LoginForm;
