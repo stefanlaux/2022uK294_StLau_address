c8ee2f3910e7f7036995bf7f7bea31e63f6292a5
StefanLaux <stefan.laux@edu.tbz.ch>
2022-06-02 12:15:52 +0200

    Add jwt button molekul

---
+++ Projektarbeit/react-app/src/Frontend/molecules/JWTButton.tsx
@@ -0,0 +1,12 @@
+import '../css/App.css';
+
+export default function jwt() {
+
+
+    return(
+        <div>
+            <img className="jwt" src="https://vegibit.com/wp-content/uploads/2018/07/JSON-Web-Token-Authentication-With-Node.png"></img>
+        </div>
+
+    );
+}
--- Projektarbeit/react-app/src/Frontend/pages/Login.tsx
+++ Projektarbeit/react-app/src/Frontend/pages/Login.tsx
@@ -1,10 +1,12 @@
 import '../css/App.css';
+import JWT from '../molecules/JWTButton';
 import LoginForm from '../organisms/LoginForm';
 
 function Login() {
   return (
     <div className="App">
       <LoginForm/>
+      <JWT/>
     </div>
   );
 }
